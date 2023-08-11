a0d7fce3c7c9d62c6f1080dfe1463cf7a2773663
Salma NAFFAH <109987748+salmanfh42@users.noreply.github.com>
2023-08-11 15:27:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    return {
+        restrict : 'E',
+        templateUr : 'modulesskills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: 'm'
+        }
+    };
+
+}   
